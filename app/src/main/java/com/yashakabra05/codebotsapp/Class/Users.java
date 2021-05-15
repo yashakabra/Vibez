e371bf7463472cb77f722740e08266c62b20cfeb
Anuj pandey <80450542+Anuj-pandey1@users.noreply.github.com>
2021-05-15 17:07:37 +0530

    login and createby anuj

---
+++ app/src/main/java/com/yashakabra05/codebotsapp/Class/Users.java
@@ -0,0 +1,54 @@
+package com.yashakabra05.codebotsapp.Class;
+
+public class Users {
+
+    String name, email, uid, location, phone;
+
+    public Users(String name, String email, String uid, String location, String phone) {
+        this.name = name;
+        this.email = email;
+        this.uid = uid;
+        this.location = location;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
