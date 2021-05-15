aeb29d148f72a8631232694d513f170465f4f01c
yashakabra <yashakabra05@gmail.com>
2021-05-15 16:21:59 +0530

    final design 1

--- app/src/main/java/com/yashakabra05/codebotsapp/EventInformation.java
+++ app/src/main/java/com/yashakabra05/codebotsapp/EventInformation.java
@@ -6,7 +6,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,8 +18,8 @@ public class EventInformation  extends AppCompatActivity  {
 
     ArrayList<event_details> ed = new ArrayList<>();
 
-     TextView name,date,time,info,t_cost;
-     ImageView ivimage;
+     TextView name,date,time,info,t_cost,tvlocation;
+     ImageView ivimage, ivlocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,8 @@ public class EventInformation  extends AppCompatActivity  {
         info = findViewById(R.id.tvinfo);
         t_cost = findViewById(R.id.tvprice);
         ivimage = findViewById(R.id.ivimage);
+        tvlocation = findViewById(R.id.tvlocation);
+        ivlocation = findViewById(R.id.ivlocation);
 
         ed.add(new event_details("MUSIC CONCERT","12/05/2021","5.00 PM","We present you here an event management cum assistance app which not only provides a\n" +
                 "hassle free environment to the event manager to manage his event\n" +
@@ -38,14 +42,23 @@ public class EventInformation  extends AppCompatActivity  {
                 "here our user is not just provided with an enhanced ticket booking service but also\n" +
                 "a complete  guidance and assistance throughout the event like cab booking service right \n" +
                 "at a click, a complete map of the event spot for knowing the location of basic nessisities\n" +
-                "and the most important services like emergeny ","500 RS","PARADE GROUND , AGRA","pic1"));
+                "and the most important services like emergeny ","500 RS","taj mahal AGRA","pic1"));
 
         name.setText(ed.get(0).getName());
         date.setText(ed.get(0).getDate());
         time.setText(ed.get(0).getTime());
         info.setText(ed.get(0).getInfo());
         t_cost.setText(ed.get(0).getT_cost());
+        tvlocation.setText(ed.get(0).getLocation());
         ivimage.setImageResource(R.drawable.mercedes);
+
+        ivlocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentb = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + tvlocation));
+                startActivity(intentb);
+            }
+        });
 
 
 
