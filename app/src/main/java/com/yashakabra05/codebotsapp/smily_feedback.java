d27209eb7c90b475c8fae00036956945cb7cd238
harsh <harsh.20205072@mnnit.ac.in>
2021-05-17 11:28:41 +0530

    login pages ready with modified help feedback about

--- app/src/main/java/com/yashakabra05/codebotsapp/smily_feedback.java
+++ app/src/main/java/com/yashakabra05/codebotsapp/smily_feedback.java
@@ -8,14 +8,15 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.hsalf.smilerating.SmileRating;
-import com.hsalf.smileyrating.SmileyRating;
 
 public class smily_feedback extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {//humei cheeze show krni h isliye hum override wali mei likhenge jo aayega
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feedback);
         //message string ko nikalo main activity se msg ke througj
+
     }
 }
