f2d64e11bc20af9785bbb5ba4b1b6484217322d7
yashakabra <yashakabra05@gmail.com>
2021-05-21 22:24:17 +0530

    FINAL APP READY......

--- app/src/main/java/com/yashakabra05/codebotsapp/CreateActivity.java
+++ app/src/main/java/com/yashakabra05/codebotsapp/CreateActivity.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -44,8 +45,35 @@ public class CreateActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                auth.createUserWithEmailAndPassword(etmail2.getText().toString(),etpswd2.getText().toString())
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                auth.createUserWithEmailAndPassword(etmail2.getText().toString(),etpswd2.getText().toString()).
+                        addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull  Task<AuthResult> task) {
+                                auth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()){
+
+                                            SharedPreferences shrd = getSharedPreferences("data", MODE_PRIVATE);
+                                            SharedPreferences.Editor editor = shrd.edit();
+
+                                            editor.putString("email", etmail2.getText().toString());
+                                            editor.putString("name", etname2.getText().toString());
+                                            editor.putString("password", etpswd2.getText().toString());
+                                            editor.apply();
+
+                                            Intent intent = new Intent(CreateActivity.this, EnterActivity.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                        else {
+                                            Toast.makeText(CreateActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                            }
+                        });
+                    /*    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
 
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -68,6 +96,8 @@ public class CreateActivity extends AppCompatActivity {
                                 }
                             }
                         });
+
+                     */
             }
         });
 
